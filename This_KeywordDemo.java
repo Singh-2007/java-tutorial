f1b525ea2314e203cf34c962a497c21958717fa8
singh-2007 <rakshitsingh071@gmail.com>
2026-07-02 15:38:47 +0530

    write a program Using Super keyword

--- This_KeywordDemo.java
+++ This_KeywordDemo.java
@@ -1,5 +1,5 @@
 class Example{
-    private int x,y;
+     int x,y;
     void f1 (int x, int y){
         this.x=x;
         this.y=y;
@@ -8,14 +8,38 @@ void display(){
     
 System.out.println(x);
 System.out.println(y);
+  }
+} 
+class Example1 extends Example{
+int x , y;
+void f3 (int x, int y){
+    super.x=x;
+    super.y=y;
+   }
+   void f4 (){
+    System.out.println(super.x);
+    System.out.println(this.y);
+   }
 }
-}
-class ThisK{
-    public static void main (String[]args){
-Example e1 = new Example ();
-e1.f1 (55,66);
-e1.display(); 
-Example e2 = new Example ();
 
-    }
-}
+  class ThisK{
+      public static void main (String[]args){
+        Example1 e1 = new Example1();
+        e1.f3(7,9);
+        e1.f4();
+      }
+  }    
+
+
+
+
+
+
+
+// Example e1 = new Example ();
+//  e1.f1 (55,66);
+//  e1.display();  
+//  Example e2 = new Example ();
+
+//      }
+//  }
